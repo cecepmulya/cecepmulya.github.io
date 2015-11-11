e2b0e37fd51036fde8b677c4d77ca2fad28cc957
Cecep Mulya <mulyadicecep@gmail.com>
2015-11-12 00:56:20 +0700

    Create header.php

---
+++ jpid/header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="text/javascript">
+function tt(sn, hn) {
+var e = document.getElementById(sn);
+var f = document.getElementById(hn);
+if(e.style.display == 'inline') {
+e.style.display = 'none';
+f.style.display = 'inline';
+}
+else {
+e.style.display = 'inline';
+f.style.display = 'none';
+}
+}
+</script>
+<meta name= "keywords" content="bahasa Indonesia, bahasa Jepang">
+<meta name= "description" content="bahasa Indonesia - bahasa Jepang untuk pemula, Buku pelajaran dalam dua bahasa">
+<link rel="shortcut icon" href="http://blog.ccpro.ga/favicon.ico"/>
+<title>bahasa Indonesia - bahasa Jepang untuk pemula
+</title>
+<link href="http://www.goethe-verlag.com/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<style type="text/css">
+A { TEXT-DECORATION: none }
+A:visited {text-decoration: none}
+A:active {text-decoration: none}
+A:hover {text-decoration: underline; color: red;}
+
+.verlauf {
+background:-moz-linear-gradient(bottom,#f7f7f7,#ffffff); /* Firefox */
+background:-webkit-gradient(linear,left top,left bottom,from(#f7f7f7),to(#ffffff)); /* Chrome, Safari */
+filter: progid:DXImageTransform.Microsoft.gradient(GradientType=0,startColorstr='#f7f7f7', endColorstr='#ffffff');/*For IE7-8-9*/ 
+height: 1%;/*For IE7*/ }
+<!--
+.Stil35 {font-family: "Verdana"; font-size: 16px; text-align: left; margin:2px}
+.Stil36 {font-family: "Verdana"; font-size: 18px; text-align: left; margin:2px}
+.Stil45 {font-family: "'Arial Unicode MS'"; font-size: 16px; text-align: left; margin:2px}
+.Stil46 {font-family: "'Arial Unicode MS'"; font-size: 18px; text-align: left; margin:2px}
+.Stil37 {color: #999999}
+.Stil38 {font-family: "Verdana"; font-size: 9px; color: #555555}
+.Stil39 {font-family: "Verdana"; font-size: 15px;}
+.Stil40 {font-family: "Verdana"; font-size: 11px;}
+-->
+
+.table-centered
+{
+margin: 0 auto !important;
+float: none !important;
+}
+
+.flagcontainer { width:100%;margin-top:20px;margin-left:7px;}
+.flag {float:left;text-align:center;font-family: "Verdana"; font-size: 11px; color: #555555;margin-right:4px;padding-bottom:8px;}
+.clearer {clear:both;}
+</style>
+<script type="text/javascript">function FO (Adresse) {
+ mf = window.open(Adresse, "zf", "width=550,height=380,scrollbars=yes");
+mf.focus();
+}
+</script>
+</head>
+<body>
